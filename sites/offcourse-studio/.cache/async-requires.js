efda56e8c643c1ef4ecdc0166d567ddc61ddf97d
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-07-17 21:51:43 +0200

    add travis file

--- sites/offcourse-studio/.cache/async-requires.js
+++ sites/offcourse-studio/.cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-templates-home-page-tsx": () => import("/home/yeehaa/Documents/offcourse/sites/offcourse-studio/src/templates/HomePage.tsx" /* webpackChunkName: "component---src-templates-home-page-tsx" */),
-  "component---cache-dev-404-page-js": () => import("/home/yeehaa/Documents/offcourse/sites/offcourse-studio/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
+  "component---src-templates-home-page-tsx": () => import("/home/yeehaa/Documents/offcourse/sites/offcourse-studio/src/templates/HomePage.tsx" /* webpackChunkName: "component---src-templates-home-page-tsx" */)
 }
 
