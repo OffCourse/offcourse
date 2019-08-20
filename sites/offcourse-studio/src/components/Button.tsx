2da5436d2803031adce794327d1e6b27c2edd7a4
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-08-20 16:30:08 +0200

    wip: contact form

--- sites/offcourse-studio/src/components/Button.tsx
+++ sites/offcourse-studio/src/components/Button.tsx
@@ -17,9 +17,9 @@ export default styled(Button)`
   border: 0;
   border-bottom: 0.125rem solid;
   padding: ${({ theme }) => `${theme.space[4]} ${theme.space[6]}`};
-  background-color: ${({ theme }) => theme.grayScale[4]};
-  border-color: ${({ theme }) => theme.colors.yellow};
-  color: ${({ theme }) => theme.grayScale[0]};
+  background-color: ${({ theme }) => theme.colors.yellow};
+  color: ${({ theme }) => theme.grayScale[4]};
+  border-color: ${({ theme }) => theme.grayScale[4]};
   font-family: ${({ theme }) => theme.fonts.heading};
   font-size: ${({ theme }) => theme.fontSizes[1]};
   line-height: ${({ theme }) => theme.lineHeights[2]};
@@ -34,9 +34,9 @@ export default styled(Button)`
   }
 
   &:hover {
-    background-color: ${({ theme }) => theme.colors.yellow};
-    color: ${({ theme }) => theme.grayScale[4]};
-    border-color: ${({ theme }) => theme.grayScale[4]};
+    background-color: ${({ theme }) => theme.grayScale[4]};
+    border-color: ${({ theme }) => theme.colors.yellow};
+    color: ${({ theme }) => theme.grayScale[0]};
   }
   @media only screen and (min-width: ${({ theme }) => theme.breakpoints[1]}) {
     width: 8rem;
