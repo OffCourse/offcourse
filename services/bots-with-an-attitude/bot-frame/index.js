976f90c9ffff81aa797c3ce109c95e7173708910
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-10-17 22:51:08 +0200

    refactor all

--- services/bots-with-an-attitude/bot-frame/index.js
+++ services/bots-with-an-attitude/bot-frame/index.js
@@ -1,25 +1,29 @@
-const bot = require("bbot");
+const bbot = require("bbot");
 const recommendCassette = require("./cassettes/recommend");
 const testCassette = require("./cassettes/test");
 
-const initiateBot = cassettes => {
-  cassettes.map(({ verb, objects, run }) => {
-    bot.global.text(
-      new RegExp(`${verb} .* (${objects.join("|")})`, "i"),
-      async b => {
-        const matchedObject = b.conditions.captures[0];
-        const recommendations = await run({
-          objectType: matchedObject
-        });
-        return b.reply(JSON.stringify(recommendations, null, 2));
-      },
-      {
-        id: "recommendations-direct"
-      }
-    );
-  });
-
-  bot.start();
+const initializeCassette = ({ verb, objects, run, scope, logger }) => {
+  scope.text(
+    new RegExp(`${verb} (${objects.join("|")})`, "i"),
+    async b => {
+      const matchedObject = b.conditions.captures[0];
+      const recommendations = await run({
+        objectType: matchedObject
+      });
+      return b.reply(JSON.stringify(recommendations, null, 2));
+    },
+    {
+      id: `${verb}-cassette`
+    }
+  );
 };
 
-initiateBot((cassettes = [recommendCassette, testCassette]));
+const initiateBot = cassettes => {
+  const { global, logger } = bbot;
+  cassettes.map(cassette =>
+    initializeCassette({ scope: global, logger, ...cassette })
+  );
+  bbot.start();
+};
+
+initiateBot([recommendCassette, testCassette]);
