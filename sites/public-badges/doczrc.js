d7910287f0f050723757b7aa8e76891afa9d7626
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-11-06 12:19:30 +0100

    changes

--- sites/public-badges/doczrc.js
+++ sites/public-badges/doczrc.js
@@ -1,5 +1,9 @@
+import mermaid from "remark-mermaid";
+import graphViz from "remark-graphviz";
+
 export default {
   typescript: true,
+  mdPlugins: [mermaid, graphViz],
   themeConfig: {
     mode: "dark"
   }
