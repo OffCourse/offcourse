04511e0b329ec4711dc3261cf118b67315d70c25
Jan Hein Hoogstad <yeehaa@offcourse.io>
2019-11-07 11:42:31 +0100

    working bot

--- sites/bots-with-an-attitude/api/index.js
+++ sites/bots-with-an-attitude/api/index.js
@@ -1,9 +1,13 @@
+const { WebAdapter } = require("botbuilder-adapter-web");
 const { Botkit } = require("botkit");
 
+const adapter = new WebAdapter();
 const controller = new Botkit({
-  webhook_uri: "/api/messages"
+  webhook_uri: "/api/messages",
+  adapter
 });
 
-controller.on("event", async (bot, message) => {
+controller.on("message", async (bot, message) => {
+  console.log(message);
   await bot.reply(message, "I received an event of type " + message.type);
 });
