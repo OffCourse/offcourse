2cf565598133fc07dacb14b1401b945a1f6d4b95
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-01-19 23:21:16 +0100

    rename api to backend

--- projects/public-badges/services/api/dist/resolvers/Scalars/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_scalars_1 = require("graphql-scalars");
-exports.EmailAddress = graphql_scalars_1.EmailAddressResolver;
-exports.GUID = graphql_scalars_1.GUIDResolver;
-exports.URL = graphql_scalars_1.URLResolver;
-exports.JSON = graphql_scalars_1.JSONResolver;
-//# sourceMappingURL=index.js.map
